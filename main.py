0bd28000de54ca92939a09d7e65d358d56314b31
Luka Anthony <lukaranthony@gmail.com>
2025-01-28 08:40:26 -0800

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import requests
 
-
 file_paths = {
     'compounds_enzyme': 'data/raw/FlavorDB/CompoundsEnzyme.csv',
     'compounds_flavor': 'data/raw/FlavorDB/CompoundsFlavor.csv',
@@ -10,6 +9,7 @@ file_paths = {
     'flavor': 'data/raw/FlavorDB/Flavor.csv',
     'food': 'data/raw/FlavorDB/Food.csv'
  }
+
 # Load data into dataframes to compress into a manageable piece
 dataframes = {name: pd.read_csv(path) for name, path in file_paths.items()}
 
@@ -21,45 +21,13 @@ nutritionix_app_key = '64a2364aa967fdf9e38dec6d157cbbb4'
 def get_nutritionix_data(query):
     url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
     headers = {
-<<<<<<< HEAD
         'x-app-id': nutritionix_app_id,
         'x-app-key': nutritionix_app_key,
         "Content-Type": 'application/json'
     }
-    response = requests.post(url, headers=headers, json={"query": query})
-    return response.json()
-
-# function to defind endpoint of APi as well as fetching the data and getting it ready to put into a dataframe
-def get_bulk_nutritionix_data(queries):
-    all_nutrition_data = []
-    for query in queries:
-        nutrition_data = get_nutritionix_data
-        all_nutrition_data.extend(nutrition_data)
-    return all_nutrition_data
-    
-    response = requests.post(url + request, headers=headers, json=query)
-    return response.json()
-
-
-def get_combined_data():
-    #combining all the data from fooDB into one dataframe to simplify the data
-    combined_df= pd.concat([dataframes['compounds_enzyme'], dataframes['compounds_flavor'], dataframes['enzyme'], dataframes['enzyme_synonym'], dataframes['flavor'], dataframes['food']])
-    #example query
-    nutrition_query = "apple" #example query, create function get bulk nutrition data here 
-    nutrition_data = get_nutritionix_data(nutrition_query)
-    #nutritionix data normalized to dataframe
-    nutrition_df = pd.json_normalize(nutrition_data)
-    #combines both dataframes into one
-    final_combined_df = pd.concat([combined_df, nutrition_df], ignore_index=True)
-    return final_combined_df
-=======
-        "Content-Type": "application/json",
-        "x-app-id": nutritionix_app_id,
-        "x-app-key": nutritionix_app_key
-}
     body = {
-        "query": query
-}
+        "query": query,
+        }
     response = requests.post(url, headers=headers, json=body)
     if response.status_code == 200:
         #tuple-data structure has multiple data pieces, returns response
@@ -67,6 +35,7 @@ def get_combined_data():
     else:
         print(f"Error handling query;{query}: {response.status_code}")
 
+# function to defind endpoint of APi as well as fetching the data and getting it ready to put into a dataframe
 def get_bulk_nutritionix_data(queries):
     responses = []
     for query in queries: 
@@ -78,13 +47,20 @@ def get_bulk_nutritionix_data(queries):
 # Select only the `food_name` and `full_nutrients` columns
     final_df = normalized_data[["food_name", "full_nutrients"]]
     return(final_df)
->>>>>>> f6d8d98fc0db30d6ad1b8b99f342342e7167903a
 
-           
+bulk_data = get_bulk_nutritionix_data('food_name')
+nutrition_df = pd.json_normalize(bulk_data["full_nutrients"])
+
+filtered_df = nutrition_df[nutrition_df
+            (nutrition_df['full_nutrients'].apply(lambda x: any(nutrient['attr_id']in [324,301,303]for nutrient in x)))
+            ]
+merged_df=pd.merge(filtered_df, dataframes ['food'], left_on='food_name', right_on='name', how='inner')
+print(merged_df)
+
+# Define the queries to be made        
 queries = ["apple", "banana", "orange", "grape", "strawberry"]
 get_bulk_nutritionix_data(queries).to_csv("bulk_nutritionix_data.csv", index=False)
 
-#next snake case the columns
 
 
  
